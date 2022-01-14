8f0a0544d2b35b27828e83c88bf9f3539ad9fab6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-01-14 09:50:43 -0600

    working on page setup

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <Container>
+    <Container className="my-4">
       <Stack direction="horizontal" gap="2" className="mb-4">
         <h1 className="me-auto">Budgets</h1>
         <Button variant="primary">Add Budget</Button>
         <Button variant="outline-primary">Add Expense</Button>
       </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      ></div>
     </Container>
   );
 }
